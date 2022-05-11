d85bf12d3ca29cdcfdcc01a5ebf09086a89bfb4f
emanuellresende <marechalmelo@gmail.com>
2022-05-11 18:24:44 -0300

    creating component map and set your functions

---
+++ src/components/Map/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+// import { Container } from './styles';
+
+function Map() {
+  const ref = useRef(null)
+  const [map, setMap] = useState();
+  return <div />;
+}
+
+export default Map;
--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 // import { Container } from './styles';
 
 function HomeScreen() {
-  return <div />;
+  return(
+    <Wrapper apiKey={"AIzaSyAsWaPP5dQAMBu6ovfr-0XgMoTAP1FTF-g"}>
+</Wrapper>
+  );
 }
 
 export default HomeScreen;
